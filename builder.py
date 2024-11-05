6a73b330557e2717129e1c9d1c47a7b8f17b382d
n4n5 <its.just.n4n5@gmail.com>
2024-11-05 18:48:26 +0100

    more understandable

--- builder.py
+++ builder.py
@@ -1,3 +1,5 @@
+"""builder https://github.com/Its-Just-Nans/coreutils-tracking-details"""
+
 import json
 from pathlib import Path
 import re
@@ -63,8 +65,10 @@ END_INDEX = """
 </html>
 """
 
+
 # Function to generate the HTML content
 def generate_html(data):
+    """Generate the HTML content for the test results"""
     html_content = f"""
     {START_INDEX}
     <h1>Test Results</h1>
@@ -90,7 +94,11 @@ def generate_html(data):
     for category, logs in data.items():
         for log_file, status in logs.items():
             status_class = status.lower()  # use status as class for styling
-            name = f"<a href='{log_file}.html'>{log_file}</a>" if status != "PASS" else log_file
+            name = (
+                f"<a href='{log_file}.html'>{log_file}</a>"
+                if status != "PASS"
+                else log_file
+            )
             html_content += f"""
             <tr class="{status_class}">
                 <td>{category}</td>
@@ -109,6 +117,7 @@ def generate_html(data):
 
 
 def decode_logs_file():
+    """Decode the logs file and return a dictionary with the results"""
     results = {}
     for one_file in ["test-suite.log", "test-suite-root.log"]:
         with open(one_file, "rb") as f:
@@ -117,7 +126,7 @@ def decode_logs_file():
         lines = lines.decode("utf-8", "ignore")
         for regex in [r"^SKIP: .*\n=*(.|\n)*?SKIP .*", r"^FAIL: .*\n=*(.|\n)*?FAIL .*"]:
             matches = re.finditer(regex, lines, re.MULTILINE)
-            for matchNum, match in enumerate(matches, start=1):
+            for _, match in enumerate(matches, start=1):
                 one_res = match.group()
                 res = one_res.split("\n")
                 test_file = res[-1].split(" ")[1]
@@ -129,27 +138,47 @@ def decode_logs_file():
     return results
 
 
+def improve_text_out(text_out):
+    """Improve the text output"""
+    rgx = re.compile(r"^(.*?\.\.\.)", re.MULTILINE)
+    text_out = "\n".join(
+        [
+            re.sub(rgx, "\n\n\g<1>\n\n\n", text_part_out)
+            for text_part_out in text_out.split("\n")
+        ]
+    )
+    return text_out
+
+
 def html_test_output(name, one_output):
+    """Generate the HTML output for a test"""
     text_out, test_file, res = one_output
+    text_out_cleaned = improve_text_out(text_out)
     res_print = res if res != 0 else ""
     txt = START_INDEX
     txt += f"<h2>{name} ({res_print} failed)</h2>\n"
     link = f"https://github.com/coreutils/coreutils/blob/master/{test_file}"
     txt += f"<a href='{link}' target='_blank'>test link: {test_file}</a>"
-    txt += f"<pre>{text_out}</pre>"
+    txt += f"<pre>{text_out_cleaned}</pre>"
     txt += END_INDEX
-    output_path = Path(f"dist/{test_name}.html")
-    output_path.write_text(txt)
+    output_path = Path(f"dist/{name}.html")
+    output_path.write_text(txt, encoding="utf-8")
 
 
-if __name__ == "__main__":
+def main():
+    """Main function"""
     makedirs("dist", exist_ok=True)
-    json_data = json.loads(Path("gnu-full-result.json").read_text())
+    json_data = json.loads(Path("gnu-full-result.json").read_text(encoding="utf-8"))
     html_content = generate_html(json_data)
 
     # Write the HTML content to an index.html file
     output_path = Path("dist").joinpath("index.html")
-    output_path.write_text(html_content)
+    output_path.write_text(html_content, encoding="utf-8")
     tables = decode_logs_file()
     for test_name, logs in tables.items():
+        # print(test_name)
         html_test_output(test_name, logs)
+
+
+if __name__ == "__main__":
+    main()
